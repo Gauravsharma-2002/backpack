fbf08ebde74ee5fcdaa46c462c3abaf14ba949bc
spiderMonk <gauravmaharaj1234@gmail.com>
2024-05-08 19:50:19 +0530

    removed unecessary commits

--- src/controller/user.controler.js
+++ src/controller/user.controler.js
@@ -21,62 +21,7 @@ const registerUser = asyncHandler(async (req, res) => {
   //   // 8. check for user creation
   //   // 9. return response
 
-  // const { userName, email, fullname, password } = req.body;
-  // if ([userName, email, fullname, password].some((ele) => ele?.trim() === "")) {
-  //   throw new apiError(400, "required feild needed to be filled");
-  // }
-  // //   check if user already exist
-  // const existedUser = await User.findOne({
-  //   $or: [{ email }, { userName }],
-  // });
-
-  // if (existedUser) {
-  //   throw new apiError(409, "a user with same email or useName exist");
-  // }
-  // // check the path for avataratar and coverimage
-
-  // console.log(req.files)
-  // const avatarPath = req.files?.avatar[0]?.path;
-  // const coverImagePath = req.files?.coverImage[0]?.path;
-
-  // if (!avatarPath) {
-  //   throw new apiError(400, "avatar is mendetory");
-
-  // }
-  // //now havatare to upload on cloudinary
-  // const cloudIResavatar = await uploadOnCloudinary(avatarPath);
-  // const cloudIResCoverImage = await uploadOnCloudinary(coverImagePath);
-
-  // //check if file is uploaded at server or not
-  // if (!cloudIResavatar) {
-  //   throw new apiError(400, "teri gaand pe marunga bsdk photo de mc");
-  // }
-
-  // //now time to make entry in database
-  // const dbResponse = await User.create({
-  //   userName: userName.toLowerCase(),
-  //   email,
-  //   password,
-  //   fullname,
-  //   avatar: cloudIResavatar.url,
-  //   coverImage: cloudIResCoverImage?.url || "",
-  // });
-
-  // const isUserCreated = await User.findById(dbResponse._id).select(
-  //   "-password -refreshToken"
-  // );
-  // //now check if userCreated
-  // if (!isUserCreated) {
-  //   throw new apiError(
-  //     500,
-  //     "something happened wrong while registering user please try again"
-  //   );
-  // }
-
-  // //   now we havatare to send response in accordance for registration
-  // return res
-  //   .status(202)
-  //   .json(new ApiResponse(200, isUserCreated, "user registered successfully"));
+  
 
   const { fullname, email, password, userName } = req.body;
   console.log("82", fullname, email, password, userName);
