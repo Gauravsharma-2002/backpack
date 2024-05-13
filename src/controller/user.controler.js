36ff63e65bc24665271577e00ca631e3a8d7dcff
spiderMonk <gauravmaharaj1234@gmail.com>
2024-05-13 16:20:06 +0530

    updated generateAccessandRefreshToken method + added loginUser method + passwordChange method +update UserDetail method + updateAvatar +updateCoverimage

--- src/controller/user.controler.js
+++ src/controller/user.controler.js
@@ -6,27 +6,27 @@ import { uploadOnCloudinary } from "../utils/Cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 // generating refreshToken access token
-const generateRefreshAndAccessToken = async function (userId) {
-  //garbarr ho sakti hai
-  try {
-    const user = await User.findById(userId);
-    //dekho yaha tumhe data aa gaya launde ka
-    //ab kya karo generate kar do
-    const accessToken = await user.createAccessToken();
-    const refreshToken = await user.createRefreshToken();
-    //ab generate ho gaya hai ab ise save kar do db me
-    //  dekho aise
-    user.refreshToken = refreshToken;
-    await user.save({ validateBeforeSave: false }); //default kya hoga jab tum kuch feild ko update karoge to us wqat sare feild ke validation kickin ho jaienge aur error maar denge to validateBeforeSave ko false kar do
+// const generateRefreshAndAccessToken = async function (userId) {
+//   //garbarr ho sakti hai
+//   try {
+//     const user = await User.findById(userId);
+//     //dekho yaha tumhe data aa gaya launde ka
+//     //ab kya karo generate kar do
+//     const accessToken = await user.createAccessToken();
+//     const refreshToken = await user.createRefreshToken();
+//     //ab generate ho gaya hai ab ise save kar do db me
+//     //  dekho aise
+//     user.refreshToken = refreshToken;
+//     await user.save({ validateBeforeSave: false }); //default kya hoga jab tum kuch feild ko update karoge to us wqat sare feild ke validation kickin ho jaienge aur error maar denge to validateBeforeSave ko false kar do
 
-    return { accessToken, refreshToken };
-  } catch (error) {
-    throw new apiError(
-      504,
-      "internal Server Error while generating Refresh Token and Access token"
-    );
-  }
-};
+//     return { accessToken, refreshToken };
+//   } catch (error) {
+//     throw new apiError(
+//       504,
+//       "internal Server Error while generating Refresh Token and Access token"
+//     );
+//   }
+// };
 
 const registerUser = asyncHandler(async (req, res) => {
   //   // return res.status(200).json({
@@ -115,51 +115,135 @@ const registerUser = asyncHandler(async (req, res) => {
 // access refresh token generate  karne ka
 // send via cookois
 
+// const userLogin = asyncHandler(async (req, res) => {
+//   const { email, userName, password } = req.body;
+//   //  check karo ki kya hai if not maro sale ko
+//   if (!userName && !email) {
+//     throw new apiError(400, "email ya userName lagega");
+//   }
+//   //check karo register hai ki ni ,ni to sunao sale ko
+//   const isUser = await User.findOne({
+//     $or: [{ userName }, { email }],
+//   });
+//   if (!isUser) {
+//     throw new apiError(400, "marenge sale ja ke register karo pahle");
+//   }
+//   // password verify karne ka
+//   // yaha dekho jo isUser hai na vo ek objecjt hai db se aya hau matlab isme jo bhi user ke lie method define kare the sab hai
+//   // to use karo unhe
+//   const passwardCorrectHaiKya = await isUser.isPasswordCorrect(password);
+//   if (!passwardCorrectHaiKya) {
+//     throw new apiError(405, "marenge 10 ginenge 1 sahi password do bsdk");
+//   }
+//   //mano ki sahi nikal gaya passward tab kya karoge
+//   // tab refresh token generate karna
+//   // suno kya karo na ek functiono hi likh lo genereate karne ke lie
+//   const { accessToken, refreshToken } = await generateRefreshAndAccessToken(
+//     isUser._id
+//   );
+//   //dekho ab tumhare pass hai 2 options
+//   // pahla ki tum upar wael object{isUser} hai usi ko update kar do ya fir ek aur db call mar do
+//   // mai marunga as per regular copyMaster schema /db call
+//   const loggedInUser = await User.findById(isUser._id).select(
+//     "-password -refreshToken"
+//   );
+
+//   //dekho babu itna kar lia ab ye karo ki inhe cookies me bhejo
+//   //  use se pahle ye samjho ki cookies ke sath kuch aur bhi jata hai
+//   //  jise kahte hai options
+//   // ye jo Options hai vo help karte hai apne cookies ko secure karne me
+//   //  kuch aise ke inhe front end se modify kara ja sakta hai par apne ko aisa hone ni deba
+//   const option = {
+//     httpOnly: true,
+//     secure: true,
+//   };
+
+//   //  ab apne ko user ko response bhejne ka
+//   //  respone me use loggin kara dena hai
+//   return res
+//     .status(200)
+//     .cookie("accessToken", accessToken, option)
+//     .cookie("refreshToken", refreshToken, option)
+//     .json(
+//       new ApiResponse(
+//         202,
+//         {
+//           user: loggedInUser,
+//           accessToken,
+//           refreshToken,
+//         },
+//         `${isUser.fullname} is loggedIN `
+//       )
+//     );
+//   // kara dia tune logged in
+//   // ab logout kara
+//   // dekh teri lagegi
+// });
+
+const generateRefreshAndAccessToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    //
+    const accessToken = await user.createAccessToken();
+    const refreshToken = await user.createRefreshToken();
+
+    user.refreshToken = refreshToken;
+    user.save({
+      validateBeforeSave: false,
+    });
+    // console.log(accessToken, refreshToken);
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new apiError(
+      502,
+      "something went wrong while creating refresh or access token"
+    );
+  }
+};
 const userLogin = asyncHandler(async (req, res) => {
-  const { email, userName, password } = req.body;
-  //  check karo ki kya hai if not maro sale ko
-  if (!email || !userName) {
-    throw new apiError(400, "email ya userId lagega");
+  const { email, password, userName } = req.body;
+
+  if (!(email || userName)) {
+    throw new apiError(402, "kindly provide either email or userName");
   }
-  //check karo register hai ki ni ,ni to sunao sale ko
-  const isUser = await User.findOne({
-    $or: [{ userName }, { email }],
+  const user = await User.findOne({
+    $or: [{ email }, { userName }],
   });
-  if (!isUser) {
-    throw new apiError(400, "marenge sale ja ke register karo pahle");
+  if (!user) {
+    throw new apiError(402, "error while fetching data from db");
   }
-  // password verify karne ka
-  // yaha dekho jo isUser hai na vo ek objecjt hai db se aya hau matlab isme jo bhi user ke lie method define kare the sab hai
-  // to use karo unhe
-  const passwardCorrectHaiKya = await isUser.isPasswordCorrect(isUser._id);
-  if (!passwardCorrectHaiKya) {
-    throw new apiError(405, "marenge 10 ginenge 1 sahi password do bsdk");
+
+  const isCorrectPassword = await user.isPasswordCorrect(password);
+  // console.log(isCorrectPassword);
+  if (!isCorrectPassword) {
+    throw new apiError(403, "wrong password");
   }
-  //mano ki sahi nikal gaya passward tab kya karoge
-  // tab refresh token generate karna
-  // suno kya karo na ek functiono hi likh lo genereate karne ke lie
+  // console.log(user._id)
   const { accessToken, refreshToken } = await generateRefreshAndAccessToken(
-    isUser._id
+    user._id
   );
-  //dekho ab tumhare pass hai 2 options
-  // pahla ki tum upar wael object{isUser} hai usi ko update kar do ya fir ek aur db call mar do
-  // mai marunga as per regular copyMaster schema /db call
-  const loggedInUser = await User.findById(isUser._id).select(
+  // console.log("line",{ accessToken, refreshToken });
+  if (!(accessToken && refreshToken)) {
+    throw new apiError(
+      401,
+      "something goes wrong while generating access or refresh Token"
+    );
+  }
+
+  user.refreshToken = refreshToken;
+  user.save({ validateBeforeSave: false });
+
+  const loggedInUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
+  if (!loggedInUser) {
+    throw new apiError(402, "something went wrong while logging you in ");
+  }
 
-  //dekho babu itna kar lia ab ye karo ki inhe cookies me bhejo
-  //  use se pahle ye samjho ki cookies ke sath kuch aur bhi jata hai
-  //  jise kahte hai options
-  // ye jo Options hai vo help karte hai apne cookies ko secure karne me
-  //  kuch aise ke inhe front end se modify kara ja sakta hai par apne ko aisa hone ni deba
   const option = {
     httpOnly: true,
     secure: true,
   };
-
-  //  ab apne ko user ko response bhejne ka
-  //  respone me use loggin kara dena hai
   return res
     .status(200)
     .cookie("accessToken", accessToken, option)
@@ -167,58 +251,263 @@ const userLogin = asyncHandler(async (req, res) => {
     .json(
       new ApiResponse(
         202,
-        {
-          user: loggedInUser,
-          accessToken,
-          refreshToken,
-        },
-        `${isUser.fullname} is loggedIN `
+        " kaise  -> maan gaya + user logged in successfully  ",
+        { user: loggedInUser, refreshToken, accessToken }
       )
     );
-  // kara dia tune logged in
-  // ab logout kara
-  // dekh teri lagegi
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  // dekh tu aise kaise logout karaiega
-  // tujhe chaie rahiga user kar cookie ko clear karne ka
-  //  try kar hoga ni ku ki tere pass na id ha na email jaise logut ke lie chahie tha
-  // to kya karega user se logut karne ke lie form bhariyega
-  // abe ni bhai middleWare use karne ka idhar
-  // pahle design kar le ek baar use kaam ho jaiega tera
-  // usme tujhe chaiye rahega user ko verify karane ka
-
-  //ab tak tu req me user object add kar chuka hoaga
-  //ab use use kar aur user ka refresh token ura db se aur logout maar sale ko \
-
-  // req.user._id
-  await User.findByIdAndUpdate(
+  const userInfo = await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
-    }, //itna kar ke chor dia na to tujhe iske response me jo aya aur tune use kahi use karna hai to vo unupdated aiega
-    //jo value db me pahle thi vahi vali jisme refreshToken para hoga
-    //  to ussese deal karne ke lie kya karo ki agli wali feild add kar do
+    },
     {
       new: true,
     }
   );
-  // dekho ab tumhe refreshToken to uda dia db se
-  // cookies ko bhi uda do
-  // aur dhayan rakhna oprions ka
+  // console.log("loggin userInfo",userInfo);
   const option = {
     httpOnly: true,
     secure: true,
   };
-
-  return req
+  return res
     .status(200)
-    .clearCookie("accessToken", option)
-    .clearCookie("refreshToken", option)
-    .json(new ApiResponse(200, {}, "logged out successfully"));
+    .cookie("accessToken", "", option)
+    .cookie("refreshToken", "", option)
+    .json(new ApiResponse(202, "user logged out succesfully ", {}));
 });
 
-export { registerUser, userLogin,logoutUser };
+// const logoutUser = asyncHandler(async (req, res) => {
+//   // dekh tu aise kaise logout karaiega
+//   // tujhe chaie rahiga user kar cookie ko clear karne ka
+//   //  try kar hoga ni ku ki tere pass na id ha na email jaise logut ke lie chahie tha
+//   // to kya karega user se logut karne ke lie form bhariyega
+//   // abe ni bhai middleWare use karne ka idhar
+//   // pahle design kar le ek baar use kaam ho jaiega tera
+//   // usme tujhe chaiye rahega user ko verify karane ka
+
+//   //ab tak tu req me user object add kar chuka hoaga
+//   //ab use use kar aur user ka refresh token ura db se aur logout maar sale ko \
+
+//   // req.user._id
+//   await User.findByIdAndUpdate(
+//     req.user._id,
+//     {
+//       $set: {
+//         refreshToken: undefined,
+//       },
+//     }, //itna kar ke chor dia na to tujhe iske response me jo aya aur tune use kahi use karna hai to vo unupdated aiega
+//     //jo value db me pahle thi vahi vali jisme refreshToken para hoga
+//     //  to ussese deal karne ke lie kya karo ki agli wali feild add kar do
+//     {
+//       new: true,
+//     }
+//   );
+//   // dekho ab tumhe refreshToken to uda dia db se
+//   // cookies ko bhi uda do
+//   // aur dhayan rakhna oprions ka
+//   const option = {
+//     httpOnly: true,
+//     secure: true,
+//   };
+
+//   return req
+//     .status(200)
+//     .clearCookie("accessToken", option)
+//     .clearCookie("refreshToken", option)
+//     .json(new ApiResponse(200, {}, "logged out successfully"));
+// });
+
+// glitch only work when user is logged in
+// can YOU MAKE ONE TO IF USER IS NOT LOGGED IN
+const changePassword = asyncHandler(async (req, res) => {
+  // console.log("inside Change password")
+  const { oldPassword, newPassword } = req.body;
+  // verify the old password in db
+  // update the password
+  // update that in db
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new apiError(402, "no such user exists");
+  }
+  const isCorrectPassword = await user.isPasswordCorrect(oldPassword);
+  // console.log(isCorrectPassword)
+  if (!isCorrectPassword) {
+    throw new apiError(402, "wrong old password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(202, "password updated sucessfully", {}));
+});
+
+// return user if logged in
+const getUserDetail = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id).select(
+    "-password -refreshToken"
+  );
+  if (!user) {
+    throw new apiError(402, "no such user exist");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(202, "user fetched sucessfully", user));
+});
+
+const userUpdateDetail = asyncHandler(async (req, res) => {
+  const { fullname, email } = req.body;
+
+  //offcourse only logged in user can update these details
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { fullname, email },
+    },
+    {
+      new: true,
+    }
+  ).select("-password -refreshToken");
+  if (!user) {
+    throw new apiError(
+      402,
+      "something went wrong while updating , please try again"
+    );
+  }
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(202, "users detailes updated sucessfully", user));
+});
+
+const updateAvatar = asyncHandler(async (req, res) => {
+  try {
+    // console.log("inside avatar changer");
+
+    const avatarFilePath = req.file?.path;
+    // console.log(avatarFilePath);
+    if (!avatarFilePath) {
+      throw new apiError(402, "no file is obtained kindly upload a file");
+    }
+    const avatar = await uploadOnCloudinary(avatarFilePath);
+    if (!avatar) {
+      throw new apiError(
+        502,
+        "something went wrong while uploading the file kindly upload it again "
+      );
+    }
+    // console.log(avatar);
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set: {
+          avatar: avatar.url,
+        },
+      },
+      { new: true }
+    );
+    if (!user) {
+      throw new apiError(502, "something went wrong while updating avatar");
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "avatar updated succesfully", user));
+  } catch (error) {
+    return res
+      .status(404)
+      .json(
+        error.message ||
+          new apiError(
+            402,
+            "something went wrong while updating avatar and in catch block"
+          )
+      );
+  }
+});
+// const changePassword = asyncHandler(async (req, res) => {
+//   //password change karne ke lie kya kya karna rahega
+//   // old password le ke verify karna rahega
+//   //fir new us model me new password set karna rahega
+//   // fir use database me update kar dena rahega
+//   // check kar lene ka ki hashed gaya ya ni
+//   const { oldPassword, newPassword } = req.body;
+//   // req.user._id
+//   const user = await User.findById(req.user._id);
+//   if (!user) {
+//     throw new apiError(400, "no such user exist while changeing password");
+//   }
+//   // (user.password === oldPassword)
+//   const isPassWordCorrect = await user.isPasswordCorrect(oldPassword);
+//   if (!isPassWordCorrect) {
+//     throw new apiError(401, "invalid wrong password");
+//   }
+//   user.password = newPassword;
+//   await user.save({ validateBeforeSave: false });
+//   return res
+//     .status(200)
+//     .json(new ApiResponse(201, "password updated succesfully", {}));
+// });
+
+// WRITE UPDATE DETAILS logic
+
+// sun dekh Ye Yaad rakhne ka try kario ki jab bhi file change ke lie kere to septrate hi kario kuki network pe load kam rakhne ka hai apne ko
+const updateCoverImage = asyncHandler(async (req, res) => {
+  try {
+    const coverImageFilePath = req.file?.path;
+    if (!coverImageFilePath) {
+      throw new apiError(402, "kindly upload a cover image");
+    }
+    const coverimage = await uploadOnCloudinary(coverImageFilePath);
+    if (!coverimage) {
+      throw new apiError(
+        502,
+        "something went wrong while uploading cover image"
+      );
+    }
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $set: {
+          coverimage: coverimage.url,
+        },
+      },
+      { new: true }
+    ).select("-password -refreshToken");
+    if (!user) {
+      throw new apiError(
+        503,
+        "something wrong happened while uploading Yoyr cover image"
+      );
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(202, "cover image updated succesfully", user));
+  } catch (error) {
+    throw new apiError(
+      501,
+      "cover image cannot be updated at this moment kindly try again after a cetrain time"
+    );
+  }
+});
+export {
+  registerUser,
+  userLogin,
+  logoutUser,
+  changePassword,
+  getUserDetail,
+  userUpdateDetail,
+  updateAvatar,
+  updateCoverImage,
+};
